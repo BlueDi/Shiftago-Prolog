299279e4af605127e77ccc93d031aaaed7696f4b
Diogo Cruz <diogohmcruz@hotmail.com>
2017-10-10 14:14:48 +0100

    Input de escolha do Board
    O User escolhe o tabuleiro com que vai jogar.
    Reduzi o código duplicado. Agora o tabuleiro onde se vai jogar está nos argumentos.

--- main.pl
+++ main.pl
@@ -1,8 +1,13 @@
 :- include('objects.pl').
 :- include('board.pl').
 
-play(normal):-
-	board(Board),
+shiftago(Empty):-
+	write('Please pick a board to play: normal, or mini\n'),
+	read(BoardName),
+	((BoardName = 'normal', board(Board)); (BoardName = 'mini', miniboard(Board))),
+	play(Board).
+
+play(Board):-
 	place_piece(Board, p1, left, 2, Board2),
 	place_piece(Board2, p2, right, 2, Board3),
 	place_piece(Board3, p1, right, 6, Board4),
@@ -14,13 +19,6 @@ play(normal):-
 	place_piece(Board9, p2, left, 5, Board10),
 	place_piece(Board10, p2, left, 5, Board11),
 	place_piece(Board11, p2, left, 5, Board12),
-	place_piece(Board12, p2, bottom, 5, NewBoard),
-	display_board(NewBoard).
-play(mini):-
-	miniboard(Board),
-	place_piece(Board, p1, left, 2, Board2),
-	place_piece(Board2, p2, right, 2, Board3),
-	place_piece(Board3, p1, right, 6, Board4),
-	place_piece(Board4, p2, right, 2, Board5),
-	place_piece(Board5, p2, top, 2, NewBoard),
-	display_board(NewBoard).
+	place_piece(Board12, p2, bottom, 5, Board13),
+	place_piece(Board13, p1, bottom, 5, Board14),
+	display_board(Board14).
