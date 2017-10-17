de2faad4e687a69048b57e7cecbc2be3d79a8c86
dfreddy <fred.p.p.r@gmail.com>
2017-10-17 19:59:48 +0100

    End Condition Done
    apenas a função que encontra o vencedor não o está a encontrar. falta
    testar mais.

--- main.pl
+++ main.pl
@@ -10,17 +10,27 @@ shiftago(Winner):-
 
 play(Board, Winner, Player):-
 	display_board(Board),
+	
+	/* ask for and execute move */
 	write('\nPlayer '), write(Player), write("'s turn\n"),	
 	write('Please pick a move (ex: l2, r2, b5, t6, etc.)\n'),
 	read(Move),
 	move_input(Move, Cardinal, Placement_N),
 	place_piece(Board, Player, Cardinal, Placement_N, NewBoard),
-	switch_player(Player, NewPlayer),
-	play(NewBoard, Winner, NewPlayer),
-	/* falta parar quando um player ganha */
-	winner(NewBoard, Winner).
 	
-	/*
+	/* check end condition */
+	winner(NewBoard, Winner),
+	write('\nWinner = '), write(Winner), write('\n\n'),
+	(
+		 Winner \= 'none' ->
+			(
+				write('GAME OVER\n\n')
+			);
+			switch_player(Player, NewPlayer),
+			play(NewBoard, Winner, NewPlayer)
+	).
+		
+	/**
 	place_piece(Board, p1, left, 2, Board2),
 	place_piece(Board2, p2, right, 2, Board3),
 	place_piece(Board3, p1, right, 6, Board4),
@@ -36,6 +46,3 @@ play(Board, Winner, Player):-
 	place_piece(Board13, p1, bottom, 5, Board14),
 	display_board(Board14),
 	*/
-
-checkWinner(Board, Winner):-
-	winner(Board, Winner).
