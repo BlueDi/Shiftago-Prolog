c561d35372332750294454e4189c1d5b4c8ebafe
Diogo Cruz <diogohmcruz@hotmail.com>
2017-10-29 17:29:03 +0000

    Winner by Column
    Já verifica se há um vencedor por coluna.
    Também removi do objects o move_inputs que não vai ser usado.
    O switch_player devia estar no main.

--- game.pl
+++ game.pl
@@ -1,6 +1,8 @@
+:- use_module(library(lists)).
+
 winner([Line|OtherLines], Winner):-
 	(winner_lines([Line|OtherLines], Winner), Winner \= e);
-	%winner_column([Line|OtherLines], Winner);
+	(winner_column([Line|OtherLines], Winner), Winner \= e);
 	Winner = 'none'.
 
 winner_lines([], _).
@@ -19,5 +21,12 @@ count_elem(X, [Y|Lista], N):-
 count_elem(X, [X|Lista], N):-
 	count_elem(X, Lista, N1),
 	N is N1 + 1.
-	
-winner_column(_, _). % TODO
+
+winner_column([Line|OtherLines], Winner):-
+	nth1(N, Line, Winner),
+	Winner \= e,
+	winner_column(N, OtherLines, Winner).
+winner_column(_, [], _).
+winner_column(N, [Line|OtherLines], Winner):-
+	nth1(N, Line, Winner),
+	winner_column(N, OtherLines, Winner).
--- objects.pl
+++ objects.pl
@@ -21,36 +21,6 @@ translate(x, 'X').
 translate(p1, '1').
 translate(p2, '2').
 
-/* Move inputs */
-move_input(l1, left, 1).
-move_input(l2, left, 2).
-move_input(l3, left, 3).
-move_input(l4, left, 4).
-move_input(l5, left, 5).
-move_input(l6, left, 6).
-move_input(l7, left, 7).
-move_input(r1, right, 1).
-move_input(r2, right, 2).
-move_input(r3, right, 3).
-move_input(r4, right, 4).
-move_input(r5, right, 5).
-move_input(r6, right, 6).
-move_input(r7, right, 7).
-move_input(t1, top, 1).
-move_input(t2, top, 2).
-move_input(t3, top, 3).
-move_input(t4, top, 4).
-move_input(t5, top, 5).
-move_input(t6, top, 6).
-move_input(t7, top, 7).
-move_input(b1, bottom, 1).
-move_input(b2, bottom, 2).
-move_input(b3, bottom, 3).
-move_input(b4, bottom, 4).
-move_input(b5, bottom, 5).
-move_input(b6, bottom, 6).
-move_input(b7, bottom, 7).
-
 /* Switch Player */
 switch_player(Player, NewPlayer):-
 	(NewPlayer = 'p2', Player \= 'p2');
