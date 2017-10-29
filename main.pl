f1fb80b8e3762f63dbad8709dd2aded2650f7c02
Diogo Cruz <diogohmcruz@hotmail.com>
2017-10-29 08:55:18 +0000

    GameMode Implementation

--- main.pl
+++ main.pl
@@ -1,48 +1,49 @@
+:- use_module(library(random)).
+
 :- include('objects.pl').
 :- include('board.pl').
 :- include('game.pl').
 
+cardinal_moves([top, left, right, bottom]).
+
 shiftago(Winner):-
-	write('Please pick a board to play: normal, or mini\n'),
+	write('Please pick a board to play [normal, mini]'), nl,
 	read(BoardName),
 	((BoardName = 'normal', board(Board)); (BoardName = 'mini', miniboard(Board))),
-	play(Board, Winner, p1).
-
-play(Board, Winner, Player):-
-	display_board(Board),
 	
-	/* ask for and execute move */
-	write('\nPlayer '), write(Player), write("'s turn\n"),	
-	write('Please pick a move (ex: l2, r2, b5, t6, etc.)\n'),
-	read(Move),
-	move_input(Move, Cardinal, Placement_N),
-	place_piece(Board, Player, Cardinal, Placement_N, NewBoard),
+	write('Please pick a game mode: CPU vs CPU, CPU vs Human, Human vs Human [cc, ch, hh]'), nl,
+	read(GameMode),
+	(GameMode = 'cc'; GameMode = 'ch'; GameMode = 'hh'),
+	
+	play(Board, GameMode, Winner, p1).
+
+play(Board, GameMode, Winner, Player):-
+	display_board(Board),	
+	
+	nl, write('Player '), write(Player), write(' turn'), nl,
+	length(Board, BoardSize),
+	get_move(GameMode, BoardSize, Cardinal, Position),
+	place_piece(Board, Player, Cardinal, Position, NewBoard),
 	
 	/* check end condition */
 	winner(NewBoard, Winner),
-	write('\nWinner = '), write(Winner), write('\n\n'),
+	nl, write('Winner = '), write(Winner), nl, nl,
 	(
 		 Winner \= 'none' ->
 			(
-				write('GAME OVER\n\n')
+				write('GAME OVER'), nl, nl
 			);
 			switch_player(Player, NewPlayer),
-			play(NewBoard, Winner, NewPlayer)
+			play(NewBoard, GameMode, Winner, NewPlayer)
 	).
-		
-	/**
-	place_piece(Board, p1, left, 2, Board2),
-	place_piece(Board2, p2, right, 2, Board3),
-	place_piece(Board3, p1, right, 6, Board4),
-	place_piece(Board4, p2, right, 2, Board5),
-	place_piece(Board5, p2, top, 2, Board6),
-	place_piece(Board6, p2, bottom, 5, Board7),
-	place_piece(Board7, p2, bottom, 5, Board8),
-	place_piece(Board8, p2, left, 5, Board9),
-	place_piece(Board9, p2, left, 5, Board10),
-	place_piece(Board10, p2, left, 5, Board11),
-	place_piece(Board11, p2, left, 5, Board12),
-	place_piece(Board12, p2, bottom, 5, Board13),
-	place_piece(Board13, p1, bottom, 5, Board14),
-	display_board(Board14),
-	*/
+
+get_move(hh, BoardSize, Cardinal, Position):-
+	write('Please pick a side [top, left, right, bottom]'), nl,
+	read(Cardinal), % TODO: Check if Cardinal is valid
+	write('Please pick a position [1, Board_Size]'), nl,
+	read(Position). % TODO: Check if Position is valid
+get_move(cc, BoardSize, Cardinal, Position):-
+	cardinal_moves(AllCardinal),
+	random_member(Cardinal, AllCardinal),
+	BoardSize1 is BoardSize + 1,
+	random(1, BoardSize1, Position).
